9256b53ef62e18e6513a204bb5db8340c055fe95
DanielTreep <danieltreep@live.nl>
2021-12-02 20:44:09 +0100

    added charts

--- js/app.js
+++ js/app.js
@@ -1,17 +1,122 @@
+// Create alert banner
 const alertBanner = document.getElementById("alert");
 
-// create the html for the banner
 alertBanner.innerHTML = `
     <div class="alert-banner">
-        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+        <p><strong>Alert:</strong> The chickens have escaped!</p>
         <p class="alert-banner-close">x</p>
     </div>
     `;
 
+// Close banner when clicked on X
 const x = document.querySelector(".alert-banner-close");
 
-x.addEventListener('click', event => {
-    const x = event.target;
+x.addEventListener('click', () => {
     alertBanner.style.display = "none"
+});
+
+// Traffic Chart
+const trafficCanvas = document.getElementById("traffic-chart");
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+};
+
+let trafficOptions = {
+    backgroundColor: 'rgba(112, 104, 201, .5)',
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+    }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+// Daily Chart
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+//  Mobile Chart
+const mobileCanvas = document.getElementById("mobile-chart");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones", "Screenreader"],
+    datasets: [{
+        
+        data: [2000, 550, 500, 50],
+        label: '# of Users',
+        borderWidth: 0,
+        backgroundColor: [
+        '#7477BF',
+        '#78CF82',
+        '#51B6C8',
+        '#C71585'
+        ]
+    }]
+};
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
